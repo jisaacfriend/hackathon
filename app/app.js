135f43d9988958503731de8486e077ffbe294153
Drew <thorsdre@gmail.com>
2016-01-21 00:26:10 -0600

    error message on bad vin

--- app/app.js
+++ app/app.js
@@ -8,10 +8,14 @@
 
 		$scope.showData = function() {
 			console.log("it ran");
+			$scope.vinVehicles = ""
 			var url = "/api/vehicles/"+$scope.vin;
 
 			$http.get(url).success( function(response) {
 				$scope.vinVehicles = response;
+				console.log($scope.vinVehicles);
+			}).error( function() {
+				$scope.vinVehicles = "No VIN by that number. Such Fail!";
 				console.log($scope.vinVehicles);
 			});
 		};
